9660a5c7a51e883105b14295e5fd546f0a70a67a
tinho1123 <tinhoefaela@gmail.com>
2022-08-23 21:12:56 -0300

    infra: sequelize

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -3,7 +3,7 @@ require('dotenv/config')
 module.exports = {
   development: {
     username: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_PASSWORD || 'Well.10091999@',
+    password: process.env.MYSQL_PASSWORD || 'Well.10091999',
     database: process.env.MYSQL_DATABASE || "DB_cdc",
     host: process.env.MYSQL_HOST || "127.0.0.1",
     dialect: "mysql"
